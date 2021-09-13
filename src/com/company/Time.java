c231f6755dfd096a669f6c2870179236cc099523
AliseBaumane <alise.adamsone@gmail.com>
2021-09-13 12:06:07 +0300

    made the assigment with all corrections (formated code)

--- src/com/company/Time.java
+++ src/com/company/Time.java
@@ -10,11 +10,11 @@ public class Time {
 
         if (time <= 12)
             System.out.println("Good Morning Sunshine!");
-        if (13 <= time && time<= 19)
+        if (13 <= time && time <= 19)
             System.out.println("Good Afternoon Work Hard");
-if (20<=time && time <=24)
-    System.out.println("Good evening. Get some rest");
-if (time<=0 || time>24)
-    System.out.println("Not correct time format");
+        if (20 <= time && time <= 24)
+            System.out.println("Good evening. Get some rest");
+        if (time <= 0 || time > 24)
+            System.out.println("Not correct time format");
     }
 }
