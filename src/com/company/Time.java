9aa0177f52705cafd2321cff326a678762f7aef3
AliseBaumane <alise.adamsone@gmail.com>
2021-09-13 12:05:32 +0300

    made the assigment with all corrections

--- src/com/company/Time.java
+++ src/com/company/Time.java
@@ -3,13 +3,18 @@ package com.company;
 public class Time {
 
     public static void main(String[] args) {
-        int time = 8;
-        if (time < 12)
-            System.out.println("Good Morning Sunshine!");
-        if (13 < time && time< 19)
-            System.out.println("Good Afternoon Work Hard");
-if (20<time && time <24)
-    System.out.println("Good evening. Get some rest");
+        //int time = 8;
+        //int time =20;
+        //int time = -2;
+        int time = 100;
 
+        if (time <= 12)
+            System.out.println("Good Morning Sunshine!");
+        if (13 <= time && time<= 19)
+            System.out.println("Good Afternoon Work Hard");
+if (20<=time && time <=24)
+    System.out.println("Good evening. Get some rest");
+if (time<=0 || time>24)
+    System.out.println("Not correct time format");
     }
 }
